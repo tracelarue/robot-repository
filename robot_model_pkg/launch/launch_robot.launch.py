64340a9bdc6537f92e948468af143feb39bd3cde
Trace LaRue Pi5 <traceglarue@gmail.com>
2025-04-21 14:50:27 -0500

    Ros2 Control on Real Robot working

--- robot_model_pkg/launch/launch_robot.launch.py
+++ robot_model_pkg/launch/launch_robot.launch.py
@@ -20,10 +20,11 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false'}.items()
+                )]), launch_arguments={'use_sim_time': 'false','use_ros2_control': 'true'}.items()
     )
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    
     controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
 
     controller_manager = Node(
