7b6d16b5a15ed8cac8f0fc8270a618baedceafce
Trace LaRue <traceglarue@gmail.com>
2025-04-21 08:55:42 -0500

    Ros2 Control Updated

--- robot_model_pkg/launch/rsp.launch.py
+++ robot_model_pkg/launch/rsp.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
@@ -14,14 +14,16 @@ def generate_launch_description():
 
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('robot_model_pkg'))
     xacro_file = os.path.join(pkg_path,'urdf','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -29,28 +31,28 @@ def generate_launch_description():
         parameters=[params]
     )
 
-    # Create RViz2 node with default configuration
-    default_rviz_config_path = os.path.join(pkg_path, 'rviz', 'default.rviz')
-    
-    # Check if the rviz config file exists, otherwise use empty config
-    rviz_config_file = default_rviz_config_path if os.path.exists(default_rviz_config_path) else ''
     
     node_rviz2 = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
-        arguments=['-d', rviz_config_file] if rviz_config_file else [],
+        arguments=[],
         parameters=[{'use_sim_time': use_sim_time}]
     )
-
+    
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
+        DeclareLaunchArgument(
+            'use_ros2_control',
+            default_value='true',
+            description='Use ros2_control if true'),
 
         node_robot_state_publisher,
         node_rviz2
+
     ])
